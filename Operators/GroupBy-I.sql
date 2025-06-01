8953ff1a51d94cfe0097fc2dc21fdef2e6dbc19b
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-06-01 23:54:10 +0530

    group by

--- Operators/GroupBy-I.sql
+++ Operators/GroupBy-I.sql
@@ -11,3 +11,26 @@ select distinct Gender from Customer;
 select distinct year_of_joining from Employee 
 where year_of_joining > 2010;
 
+select Address, count(Address) as Address_times from customer
+where City = 'Jalandhar'
+group by Address;
+
+select City, count(cname) as Number from Customer
+group by City
+order by Number desc;
+
+select City, avg(TotalOrdersYet) from Customer
+group by City;
+
+select Gender, min(TotalOrdersYet) from Customer
+group by Gender;
+
+select Pincode, max(TotalOrdersYet) from Customer
+group by Pincode;
+
+select Address, sum(TotalOrdersYet) from Customer
+group by Address;
+
+
+
+
